bff6e01d07c5bbaf53b8c8319d67a91653316417
Frank Rowe <fsrowe@gmail.com>
2017-02-13 14:07:57 -0500

    add iOS project + index.js

---
+++ ios/RNSpatialConnect/RNSpatialConnect.h
@@ -0,0 +1,22 @@
+//
+//  RNSpatialConnect.h
+//  RNSpatialConnect
+//
+//  Created by Frank Rowe on 2/13/17.
+//  Copyright © 2017 Boundless Spatial. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "RCTBridgeModule.h"
+#import "AIRMap.h"
+#import <ReactiveCocoa/RACSignal.h>
+#import <SpatialConnect/SCJavascriptBridgeAPI.h>
+#import <SpatialConnect/SpatialConnect.h>
+
+@interface RNSpatialConnect : NSObject <RCTBridgeModule> {
+  SpatialConnect *sc;
+  SCJavascriptBridgeAPI *bridgeAPI;
+  AIRMap *mapView;
+}
+
+@end
